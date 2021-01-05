8b663230ee36bcf7b9aca9dc0c35137ea21453f3
Syaamil Fa'iq <74599637+escornbar@users.noreply.github.com>
2021-01-05 17:15:28 +0800

    latest

--- Yuu-Tube/src/yuu/tube/LoginForm.java
+++ Yuu-Tube/src/yuu/tube/LoginForm.java
@@ -155,8 +155,7 @@ public class LoginForm extends javax.swing.JFrame {
         } catch (SQLException ex) {
             Logger.getLogger(RegisterForm.class.getName()).log(Level.SEVERE, null, ex);
             }
-        //YuuTube a=new YuuTube();
-        //a.home();
+        
     }//GEN-LAST:event_jButtonLoginActionPerformed
 
     private void jLabelRegisterMouseClicked(java.awt.event.MouseEvent evt) {//GEN-FIRST:event_jLabelRegisterMouseClicked
