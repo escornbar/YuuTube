dde8cb62d34700aae0dae50e5a2413bec13be882
Syaamil Fa'iq <74599637+escornbar@users.noreply.github.com>
2021-01-08 00:10:54 +0800

    latest 8/1/2021 0010 hrs

--- Yuu-Tube/src/yuu/tube/Trending.java
+++ Yuu-Tube/src/yuu/tube/Trending.java
@@ -12,18 +12,24 @@ public class Trending {
         PreparedStatement st = null; 
         ResultSet rs = null;
         conn = connection.getConnection();
-    
+
+        //get number of videos in db
         //fetch number of videos in db and sort by views in descending order
         try{
-            String SQL = "SELECT title FROM public.videostats ORDER BY views DESC LIMIT 5";
+            String SQL = "SELECT * FROM public.videostats ORDER BY views DESC LIMIT 5";
             st = conn.prepareStatement(SQL);
             rs = st.executeQuery();
             System.out.println("\n--Trending on YuuTube--");
             //loop to print top 5 videos with highest views
             for(int i=1;i<6;i++){
                 if(rs.next()) {
+                    int views = rs.getInt("views");
+                    int likes = rs.getInt("likes"); 
+                    int dislikes = rs.getInt("dislikes");
                     System.out.print("#" +i+" ");
                     System.out.println(rs.getString("title"));
+                    System.out.println("Views: "+views+" Likes: "+likes+" Dislikes: "+dislikes);
+                    System.out.println("");
                 }
             }
         }catch (SQLException ex) {
